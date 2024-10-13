31a19991c8e9d38aa3b93fa1a8316a22c64587d4
ryanho1 <ryan81645@gmail.com>
2024-10-14 03:22:23 +0800

    compelted until step 2, confused with step 3 atm

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+const Library = [];
+
 function Book(title,author,pages,read){
 this.title = title;
 this.author = author;
 this.pages = pages;
 this.read = read;
-this.info = function(){
-  return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read
+// this.info = function(){
+//   return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read
+// }
 }
-}
-Book.prototype.publisher = function () {
-  return 'published by ' + this.author;
-}
-const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '295 pages', 'not read yet');
 
-console.log('h')
+var math = new Book('Statistics','bobobo','100','not read');
+var science = new Book('biology','jojojo', '999','read');
+var history = new Book('American history 101', 'ahahhaha', '1111', 'not read');
+
+function addBookToLibrary(book){
+    Library.push(book);
+}
+addBookToLibrary(math);
+
+function bookTitleOnly(Library) {
+    while(Library.length){
+        console.table(Library)
+    }
+}
+
+bookTitleOnly(math);
+console.table(Library);
+console.log(bookTitleOnly(math))
